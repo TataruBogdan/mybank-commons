7c425d145c6004a35ea5b45b19823d1b7f3b84e7
TataruBogdan <tatarubog@gmail.com>
2021-10-29 21:00:35 +0200

    ReDone Commons and Scheduler - Change from each microservice with on database to shared database

--- src/main/java/banking/commons/dto/IndividualDTO.java
+++ src/main/java/banking/commons/dto/IndividualDTO.java
@@ -2,15 +2,30 @@ package banking.commons.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.util.Date;
 
 @Data
 public class IndividualDTO {
-    private int id;
+
+    @NotNull
+    private Integer id;
+    @NotNull
     private String firstName;
+    @NotNull
     private String lastName;
     private String address;
     private Date birthDate;
+    private String phoneNumber;
+
+    @NotNull
+    @Email(regexp = ".+@.+\\..+", message = "Please provide a valid email address")
+    private String emailAddress;
+
+    @NotNull
+    @Pattern(regexp = "^[0-9]{13}$", message = "Please provide 13 cyphers")
     private String cnp;
     private String employerName;
     private String occupation;
